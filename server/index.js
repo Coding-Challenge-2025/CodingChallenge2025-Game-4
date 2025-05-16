d18a03cae3f3d1cbd0a500a915ca5a9d1b9848e1
anhhuy007 <imanhhuy007@gmail.com>
2025-05-17 00:27:50 +0700

    feat: modified index.js

--- server/index.js
+++ server/index.js
@@ -1,36 +1,50 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import apiRoutes from './routes/api.route.js';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import apiRoutes from "./routes/api.route.js";
+import { Server } from "socket.io";
+import setupSocketServer from "./socket/socketServer.js";
+import http from "http";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+setupSocketServer(io);
+
 // middleware
 app.use(cors());
-app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.json({ limit: "1mb" }));
 app.use(express.json());
 
 // routes
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'ok',
-        message: 'VoxelCode server is running',
-     }); 
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "VoxelCode server is running",
+  });
+});
 
 // error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).json({
-        error: true,
-        message: 'Internal Server Error',
-        stack: process.env.NODE_ENV === 'development' ? err.stack : null,
-    })
-})
+  console.error(err.stack);
+  res.status(500).json({
+    error: true,
+    message: "Internal Server Error",
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+  });
+});
 
-app.listen(PORT, () => {
-    console.log(`VoxelCode server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`VoxelCode server is running on port ${PORT}`);
 });
